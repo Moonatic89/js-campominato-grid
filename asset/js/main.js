5b5fe81d0054f4a58f5eb548f942cb379cca1e25
StefanoNesi <sn.boolean@gmail.com>
2021-10-20 14:10:49 +0200

    prompt created

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+
+/*
+The user choose a difficulty level between 1 and 3.
+
+The software creates a squared grid with a range of cells based on the difficulty levels:
+ - 1 -> 1..100
+ - 2 -> 1..81
+ - 3 -> 1..49
+
+In every cell there is an incresing number.
+
+When the user clicks on a cell it turns azure.
+*/
+
+const difficulty = parseInt(prompt("Choose a difficulty lever between 1 and 3"));
